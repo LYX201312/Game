1a1d5b2c99f55ebe4ab993fbaac66d8e349df895
LYX201312 <LYX201312@outlook.com>
2026-03-12 22:32:52 +0800

    Del GL...,Added Raylib...

--- main.cpp
+++ main.cpp
@@ -1,3 +1,48 @@
-#include<GLFW/glfw3.h>
+#include <raylib.h>
 
 
+int main(){
+	InitWindow(800,600,"GrabFightEvacuate");
+	
+	InitAudioDevice();
+	
+	SetTargetFPS(120);
+	SetTraceLogLevel(LOG_ALL);
+	
+	bool isFull=false;
+	Vector2 orgposxy;
+	int orgwx=0,orgwy=0;
+	while(!WindowShouldClose()){
+		if(IsKeyPressed(KEY_F11)){
+			if(isFull){
+				ToggleBorderlessWindowed();
+				
+				SetWindowPosition(orgposxy.x,orgposxy.y);
+				SetWindowSize(orgwx,orgwy);
+			}else{
+				orgposxy=GetWindowPosition();
+				orgwx=GetRenderWidth();
+				orgwy=GetRenderHeight();
+				
+				ToggleBorderlessWindowed();
+				
+				SetWindowPosition(0,0);
+				SetWindowSize(GetScreenWidth(),GetScreenHeight());
+			}
+			isFull=!isFull;
+		}
+		
+		BeginDrawing();
+		
+		ClearBackground(RAYWHITE);
+		
+		EndDrawing();
+		
+	}
+	
+	CloseAudioDevice();
+	
+	CloseWindow(); 
+	
+	return 0;
+}
